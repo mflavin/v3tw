daf2f1300d4f1dcf19a02662d2579017d5cde948
mflavin <mflavin@hawk.iit.edu>
2022-06-11 15:30:54 -0500

    first commit

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+
+import App from "./App.vue";
+import "./index.css";
+import router from "./router";
+
+// https://vite-plugin-pwa.netlify.app/guide/auto-update.html#runtime
+import { registerSW } from "virtual:pwa-register";
+const updateSW = registerSW({
+  onOfflineReady() {},
+});
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+
+app.mount("#app");
---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { fileURLToPath, URL } from 'url';
+// https://vite-plugin-pwa.netlify.app/guide/#setup
+import { VitePWA } from 'vite-plugin-pwa';
+
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/v3/',
+  plugins: [
+    vue(),
+    // https://vite-plugin-pwa.netlify.app/guide/auto-update.html#setup
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        cleanupOutdatedCaches: false,
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
