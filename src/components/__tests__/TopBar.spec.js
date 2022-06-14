9ef4509edf3ed4c113f90674a3633db4623d5e79
mflavin <mflavin@hawk.iit.edu>
2022-06-14 14:32:32 -0500

    Avoid 404 and redirect to /

---
+++ src/components/__tests__/TopBar.spec.js
@@ -0,0 +1,27 @@
+import { describe, it, expect, vi } from 'vitest';
+import { createTestingPinia } from '@pinia/testing';
+import { mount } from '@vue/test-utils';
+import { TopBar } from '../TopBar';
+
+describe('TopBar', () => {
+  it('renders properly', () => {
+    const mockRoute = {
+      params: {
+        id: 1,
+      },
+    };
+    const mockRouter = {
+      push: vi.fn(),
+    };
+    const wrapper = mount(TopBar, {
+      global: {
+        plugins: [createTestingPinia({ createSpy: vi.fn })],
+        mocks: {
+          $route: mockRoute,
+          $router: mockRouter,
+        },
+      },
+    });
+    expect(wrapper.text()).toContain('Home');
+  });
+});
--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,10 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/:pathMatch(.*)*',
+      redirect: '/',
+    },
+    {
       path: '/',
       name: 'home',
       component: HomeView,
