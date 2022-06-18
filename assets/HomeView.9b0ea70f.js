654d7a71f839a824e9f28f9a44483c00e6622671
mflavin <mflavin@hawk.iit.edu>
2022-06-18 10:50:55 -0500

    Updates

---
+++ assets/HomeView.9b0ea70f.js
@@ -0,0 +1,1 @@
+import{T as e}from"./ToastBox.52b99f2e.js";import{_ as t,r as n,o as s,c as a,a as r}from"./main.8d785498.js";const c={name:"HomeView",components:{ToastBox:e}};function i(p,m,_,l,d,f){const o=n("ToastBox");return s(),a("div",null,[r(o,{title:"Tailwind CSS",text:"Going through the Core Concepts sections here"})])}var h=t(c,[["render",i]]);export{h as default};
