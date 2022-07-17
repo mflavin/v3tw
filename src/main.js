3a42ea113f2a219570bb5b63f9f2d1286350f13b
mflavin <mflavin@hawk.iit.edu>
2022-07-17 16:51:29 -0500

    Add pwa

--- src/main.js
+++ src/main.js
@@ -3,12 +3,6 @@ import App from '@/App.vue';
 import '@/index.css';
 import router from '@/router';
 
-// https://vite-plugin-pwa.netlify.app/guide/auto-update.html#runtime
-// import { registerSW } from 'virtual:pwa-register';
-// const updateSW = registerSW({
-//   onOfflineReady() {},
-// });
-
 // `export const createApp` is required instead of the original `createApp(App).mount('#app')`
 export const createApp = ViteSSG(
   // the root component
@@ -18,5 +12,10 @@ export const createApp = ViteSSG(
   // function to have custom setups
   ({ app, router, routes, isClient, initialState }) => {
     // install plugins etc.
+    if (!isClient) return;
+    router.isReady().then(async () => {
+      const { registerSW } = await import('virtual:pwa-register');
+      registerSW({ onOfflineReady() {}, immediate: true });
+    });
   },
-)
+);
